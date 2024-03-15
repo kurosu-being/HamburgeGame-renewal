63c0c185ae8317faae4dfcb26468dca8fd987d08
八太 亜美 (HATTA Ami) <hatta@beingcorp.co.jp>
2024-03-15 16:16:30 +0900

    #477059 結果画面のハンバーガー画像を生成する
    GameLogic
    リネームしました。
    
    ResultForm
    獲得した具材の描画を行いました。
    
    ResultForm.Designer
    PictureBoxの配置変更を行いました。

--- HamburgerGame/HamburgerGame/ResultForm.Designer.cs
+++ HamburgerGame/HamburgerGame/ResultForm.Designer.cs
@@ -28,40 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.FDish = new System.Windows.Forms.PictureBox();
+            this.FBunUnder = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.FDish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FBunUnder)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::HamburgerGame.Properties.Resources.dish_on_bun_under;
-            this.pictureBox1.Location = new System.Drawing.Point(300, 300);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 150);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 40F);
-            this.label1.Location = new System.Drawing.Point(420, 450);
+            this.label1.Location = new System.Drawing.Point(406, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 50);
+            this.label1.Size = new System.Drawing.Size(172, 57);
             this.label1.TabIndex = 1;
             this.label1.Text = "完成 !!";
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20F);
-            this.label2.Location = new System.Drawing.Point(366, 514);
+            this.label2.Location = new System.Drawing.Point(365, 491);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(283, 29);
+            this.label2.Size = new System.Drawing.Size(254, 29);
             this.label2.TabIndex = 2;
             this.label2.Text = "MainMenuFormに戻る [Enter]";
+            // 
+            // FDish
+            // 
+            this.FDish.BackColor = System.Drawing.Color.Transparent;
+            this.FDish.Image = global::HamburgerGame.Properties.Resources.dish;
+            this.FDish.Location = new System.Drawing.Point(281, 307);
+            this.FDish.Name = "FDish";
+            this.FDish.Size = new System.Drawing.Size(423, 164);
+            this.FDish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FDish.TabIndex = 0;
+            this.FDish.TabStop = false;
+            this.FDish.Visible = false;
+            // 
+            // FBunUnder
+            // 
+            this.FBunUnder.BackColor = System.Drawing.Color.Transparent;
+            this.FBunUnder.Image = global::HamburgerGame.Properties.Resources.bun_under;
+            this.FBunUnder.Location = new System.Drawing.Point(354, 255);
+            this.FBunUnder.Name = "FBunUnder";
+            this.FBunUnder.Size = new System.Drawing.Size(277, 146);
+            this.FBunUnder.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FBunUnder.TabIndex = 3;
+            this.FBunUnder.TabStop = false;
+            this.FBunUnder.Visible = false;
             // 
             // ResultForm
             // 
@@ -69,21 +84,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.FBunUnder);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.FDish);
             this.Name = "ResultForm";
             this.Text = "ResultForm";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ResultForm_KeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FDish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FBunUnder)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox FDish;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox FBunUnder;
     }
 }
--- HamburgerGame/HamburgerGame/ResultForm.cs
+++ HamburgerGame/HamburgerGame/ResultForm.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace HamburgerGame {
     public partial class ResultForm : Form {
-        public ResultForm() {
+        private readonly GameLogic FGameLogic;
+        public ResultForm(GameLogic vGameLogic) {
             InitializeComponent();
+            this.FGameLogic = vGameLogic;
+            DrawCaughtFoods();
+            this.Paint += ResultForm_Paint;
+        }
+        private void DrawCaughtFoods() {
+            int wStartY;
+            if (FGameLogic.CaughtFoodList.Count > 0 && FGameLogic.CaughtFoodList[0].FoodInfo.Name == "cheese") {
+                //最初がチーズの場合のY座標
+                wStartY = FBunUnder.Location.Y -10;
+            } else {
+                //チーズ以外の場合のY座標
+                wStartY = FBunUnder.Location.Y - 40;
+            }
+            foreach (Food wFood in FGameLogic.CaughtFoodList) {
+                wFood.Rectangle = new Rectangle(FBunUnder.Location.X, wStartY, FBunUnder.Width, FBunUnder.Height);
+                wStartY -= 25;
+            }
         }
         private void ResultForm_KeyDown(object vSender, KeyEventArgs e) {
             var wMainMenuForm = Application.OpenForms["MainMenuForm"];
@@ -20,6 +39,25 @@ namespace HamburgerGame {
                 // 現在のフォームを閉じる
                 this.Close();
             }
+        }
+        private void ResultForm_Paint(object sender, PaintEventArgs e) {
+            DrawDish(e.Graphics);
+            DrawBunUnder(e.Graphics);
+            foreach (Food wFood in FGameLogic.CaughtFoodList) {
+                wFood.Draw(e.Graphics);
+            }
+        }
+        private void DrawBunUnder(Graphics g) {
+            // bun_under イメージを描画する矩形を作成
+            Rectangle wDestinationRect = new Rectangle(FBunUnder.Location.X, FBunUnder.Location.Y, FBunUnder.Width, FBunUnder.Height);
+            // bun_under イメージを描画
+            g.DrawImage(Properties.Resources.bun_under, wDestinationRect);
+        }
+        private void DrawDish(Graphics g) {
+            // dish イメージを描画する矩形を作成
+            Rectangle wDestinationRect = new Rectangle(FDish.Location.X, FDish.Location.Y, FDish.Width, FDish.Height);
+            // dish イメージを描画
+            g.DrawImage(Properties.Resources.dish, wDestinationRect);
         }
     }
 }
